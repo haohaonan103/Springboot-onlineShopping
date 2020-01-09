d6700642b60496e3c90d446657c6fed62cc88d98
haohaonan103 <1063615654@qq.com>
2020-01-09 18:28:07 +0800

    补充couponMapper

--- goods/src/main/java/com/shop/mapper/CouponMapper.java
+++ goods/src/main/java/com/shop/mapper/CouponMapper.java
@@ -1,12 +1,22 @@
 package com.shop.mapper;
 
+import com.shop.model.Coupon;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @Mapper
 public interface CouponMapper {
     @Insert("insert into user_coupon set userId = #{userId} , couponId = #{couponId}")
     public Integer getCoupon(Long userId,Long couponId);
+
+    @Select("select * from coupon where couponId = #{couponId}")
+    Coupon getById(Long couponId);
+
+    @Select("select * from user_coupon uc left join coupon c on uc.userId = #{userId}")
+    List<Coupon> getByUserId(Long userId);
 }
